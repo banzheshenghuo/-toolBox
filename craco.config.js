73c8e09390be0b8f6c1ef7ea1aff34894507841f
antan <2271362435@qq.com>
2021-07-31 19:03:53 +0800

    chore:add github  publicpath

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,16 @@
+console.log("process.env=>", process.env);
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+  webpack: {
+    configure: {
+      output: {
+        publicPath: process.env.NODE_ENV === "development" ? "./" : "/toolBox/",
+      },
     },
   },
 };
